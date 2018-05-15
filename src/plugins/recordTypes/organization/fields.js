3e6d8700527cee7ccc8de54251cbd8cf7343d96d
Ray Lee <rhlee@berkeley.edu>
2018-05-15 16:20:13 -0700

    UCJEPS-690: Make termSource free text.

--- src/plugins/recordTypes/organization/fields.js
+++ src/plugins/recordTypes/organization/fields.js
@@ -1,10 +1,32 @@
 export default (configContext) => {
   const {
+    TextInput,
+  } = configContext.inputComponents;
+
+  const {
+    configKey: config,
+  } = configContext.configHelpers;
+
+  const {
     extensions,
   } = configContext.config;
 
   return {
     document: {
+      'ns2:organizations_common': {
+        orgTermGroupList: {
+          orgTermGroup: {
+            termSource: {
+              [config]: {
+                view: {
+                  type: TextInput,
+                  props: null,
+                },
+              },
+            },
+          },
+        },
+      },
       ...extensions['ucbnh-organization'].fields,
     },
   };
