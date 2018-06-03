3506c90d1c9fcce649b52ddb8a6410e388cd5cd9
Ray Lee <rhlee@berkeley.edu>
2018-06-02 21:47:07 -0700

    UCJEPS-709: Configure file upload dialog accepted mime types.

--- src/plugins/recordTypes/blob/index.js
+++ src/plugins/recordTypes/blob/index.js
@@ -1,9 +1,11 @@
 import content from './content';
+import fields from './fields';
 
-export default () => ({
+export default () => configContext => ({
   recordTypes: {
     blob: {
       content,
+      fields: fields(configContext),
     },
   },
 });
