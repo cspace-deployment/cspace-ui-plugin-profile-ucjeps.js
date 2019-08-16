d75dae132eeb6397a3678b9b7337563094a04928
cesarvh <cesarv.h@berkeley.edu>
2019-08-16 13:56:34 -0400

    CC-728: Added various parameters to UOCApprovalStatus

--- src/plugins/invocables/report/ucjepsUOCApprovalStatus/fields.js
+++ src/plugins/invocables/report/ucjepsUOCApprovalStatus/fields.js
@@ -4,6 +4,10 @@ export default (configContext) => {
   const {
     CompoundInput,
     OptionPickerInput,
+    TermPickerInput,
+    DateInput,
+    TextInput,
+    AutocompleteInput,
   } = configContext.inputComponents;
 
   const {
@@ -26,11 +30,83 @@ export default (configContext) => {
               defaultMessage: 'Output format',
             },
           }),
+          required: true,
           view: {
             type: OptionPickerInput,
             props: {
               source: 'uocApprovalStatusMIMETypes',
             },
+          },
+        },
+      },
+      Requester: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.ucjepsUOCApprovalStatus.Requester.name',
+              defaultMessage: 'Requester',
+            },
+          }),
+          view: {
+            type: TextInput,
+          },
+        }
+      },
+      AuthBy: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.ucjepsUOCApprovalStatus.AuthBy.name',
+              defaultMessage: 'Authorized by',
+            },
+          }),
+          view: {
+            type: AutocompleteInput,
+            props: {
+              source: 'person/local,person/shared,person/ulan,organization/local,organization/shared,organization/collective,organization/ulan',
+            },
+          },
+        },
+      },
+      AuthStatus: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.ucjepsUOCApprovalStatus.AuthStatus.name',
+              defaultMessage: 'Authorization status',
+            },
+          }),
+          view: {
+            type: TermPickerInput,
+            props: {
+              source: 'uocauthorizationstatuses',
+            },
+          },
+        },
+      },
+      StartDate: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.ucjepsUOCApprovalStatus.StartDate.name',
+              defaultMessage: 'Start date',
+            },
+          }),
+          view: {
+            type: DateInput,
+          },
+        },
+      },
+      EndDate: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.ucjepsUOCApprovalStatus.EndDate.name',
+              defaultMessage: 'End date',
+            },
+          }),
+          view: {
+            type: DateInput,
           },
         },
       },
