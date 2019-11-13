aa0a905019cbe1107c394d22637e0ce256dd9871
cesarvh <cesarv.h@berkeley.edu>
2019-11-12 16:43:25 -0800

    CC-873: Adding forgotten student template export

--- src/plugins/recordTypes/collectionobject/forms/index.js
+++ src/plugins/recordTypes/collectionobject/forms/index.js
@@ -1,9 +1,11 @@
 import collectorLabelForm from './collectorLabel';
 import defaultForm from './default';
+import studentForm from '/.studentForm';
 
 export default configContext => ({
   collectorLabel: collectorLabelForm(configContext),
   default: defaultForm(configContext),
+  student: studentForm(configContext),
   inventory: {
     disabled: true,
   },
